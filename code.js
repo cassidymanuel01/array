d1ee44c9ac0ff391520d57e72fea345d4126a55f
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-06-07 10:26:40 +0200

    https://github.com/cassidymanuel01/array.git

---
+++ code.js
@@ -0,0 +1,8 @@
+// array exercise 
+
+const bestFriends = ["Cameron","Mikhail", "Reagan", "Clayton","Daniel", "Amanda","Joshé", "Emile", "Joshua", "Bulelani"];
+
+console.log(bestFriends);
+
+console.log(bestFriends[bestFriends.length/2]);
+console.log(bestFriends[bestFriends.length-1]);
